b0ac00ba46531c661bcd3170602afbfbb421b8a2
Max Fu <mfu27@ucmerced.edu>
2024-06-28 14:52:00 -0700

    Sockets

--- src/Sockets/sockets.cpp
+++ src/Sockets/sockets.cpp
@@ -2,14 +2,20 @@
 
 //socket descriptors is a type int. 
 
+struct sockaddr {
+    unsigned short  sa_family;          // address family, AF_xxx
+    char            sa_data[14];        // 14 bytes of protocol address
+}; 
+
 struct addrinfo {
-    int             ai_flags; 
-    int             ai_family; 
-    int             ai_sockettype; 
-    int             ai_protocol; 
-    size_t          ai_addrlen; 
-    struct sockaddr *ai_addr; 
-    char            *ai_canonname; 
+    int             ai_flags;           // AI_PASSIVE, AI_CANONNAME, etc.
+    int             ai_family;          // AF_INET, AF_INET6, AF_UNSPEC
+    int             ai_sockettype;      // SOCK_STREAM, SOCK_DGRAM
+    int             ai_protocol;        // use 0 for "any"
+    size_t          ai_addrlen;         // size of ai_addr in bytes
+    struct sockaddr *ai_addr;           // struct sockaddr_in or _in6
+    char            *ai_canonname;      // full canonical hostname
     
-    struct addrinfo *ai_next; 
+    struct addrinfo *ai_next;           // linked list, next node
+
 }; 
