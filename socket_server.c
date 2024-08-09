f3a7ef101f722efaf648feff982dce793253b2b6
Max Fu <mfu27@ucmerced.edu>
2024-08-08 21:26:54 -0700

    finished binding, working on listening

--- socket_server.c
+++ socket_server.c
@@ -22,21 +22,35 @@ struct sockaddr_in { // Parallel structure to be used with exclusively IPv4 (in 
 int main() {
     int listenfd = 0, connfd = 0; // listen_file_descriptor     connection_file_descriptor 
     
+    // (Step 1:) Create a socket. 
     if (listenfd = socket(AF_INET, SOCK_STREAM, 0) == 0) {
         perror("Socke Failed: "); 
         exit(EXIT_FAILURE); 
     }
-
+    
+    // (Step 2) Creating a structure to hold address and port number for the socket server to bind to. 
     struct sockaddr_in serv_addr; // Initalizng a struct object representing server address. ( 4 fields )
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT); 
     serv_addr.sin_addr.s_addr = INADDR_ANY;  
     
+
+    // (Step 3) Binding the socket to the address and port. 
+    if(bind(listenfd, (struct sockaddr *)serv_addr, sizeof(server_addr)) < 0) { 
     
+        perror("bind failed"); 
+        close(listenfd); 
+        exit(EXIT_FAILURE); 
 
+    } 
 
+    printf("Server is bound to port %d and ready to listen\n", PORT); 
 
+    close(listenfd); 
+    return 0; 
 
 } 
+
+
 
 // socket(Specifies IPv4 or IPv6, Specifies TCP or UDP and SOCK_STREAM is TCP, 0 lets the system choose the appropriate protocol
