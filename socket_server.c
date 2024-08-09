899f9e463d1ae5a87487aa1f9e7a040b494128cc
Max Fu <mfu27@ucmerced.edu>
2024-08-08 21:55:14 -0700

    restructure required

--- socket_server.c
+++ socket_server.c
@@ -4,23 +4,27 @@
 
 
 # define PORT 8000 // Up for definition 
-# define BACKLOG 5
-
-struct in_addr {
-    in_addr_t s_addr;  // base type for internet address - in_addr & s_ddr holds 32-bit in binary representing IPv4 address
-};
 
 
-struct sockaddr_in { // Parallel structure to be used with exclusively IPv4 (in - internet, s - socket) 
-    short int           sin_family; 
-    unsigned short int  sin_port; 
-    unsigned char       sin_zero[8]; 
-    struct in_addr      sin_addr; 
-}; 
 
 
-int main() {
+
+
+int main() { 
+    struct in_addr {
+        in_addr_t s_addr;  // base type for internet address - in_addr & s_ddr holds 32-bit in binary representing IPv4 address
+    };
+
+
+    struct sockaddr_in { // Parallel structure to be used with exclusively IPv4 (in - internet, s - socket) 
+        short int           sin_family; 
+        unsigned short int  sin_port; 
+        unsigned char       sin_zero[8]; 
+        struct in_addr      sin_addr; 
+    }; 
+
     int listenfd = 0, connfd = 0; // listen_file_descriptor     connection_file_descriptor 
+    int sock_adr; 
     
     // (Step 1:) Create a socket. 
     if (listenfd = socket(AF_INET, SOCK_STREAM, 0) == 0) {
@@ -36,7 +40,7 @@ int main() {
     
 
     // (Step 3) Binding the socket to the address and port. 
-    if(bind(listenfd, (struct sockaddr *)serv_addr, sizeof(server_addr)) < 0) { 
+    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) { 
     
         perror("bind failed"); 
         close(listenfd); 
@@ -45,6 +49,10 @@ int main() {
     } 
 
     printf("Server is bound to port %d and ready to listen\n", PORT); 
+    
+    // int connect(int sockfd, struct sockaddr *serv_addr, int addrlen); 
+    if (connect(sockfd, ))
+
 
     close(listenfd); 
     return 0; 
