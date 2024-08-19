e1bfab460006c24586a90fb9e1a7f3b3c9f231f2
Max Fu <mfu27@ucmerced.edu>
2024-08-19 09:57:24 -0700

    recieve messages from one client to another

--- socketClient.c
+++ socketClient.c
@@ -8,10 +8,23 @@
 
 # define PORT 8080 // Up for definition 
 
-void *recieve_message(void *socket_desc) { 
-    int sockfd = *(int *)socket_desc; 
-    char buffer[1024]; 
-    int bytes_received; 
+void *receive_messages(void *socket_desc) {
+    int sockfd = *(int *)socket_desc;
+    char buffer[1024];
+    int bytes_received;
+
+    while ((bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytes_received] = '\0';
+        printf("Server: %s\n", buffer);
+    }
+
+    if (bytes_received == 0) {
+        printf("Server closed the connection\n");
+    } else {
+        perror("recv failed");
+    }
+
+    return NULL;
 }
 
 
