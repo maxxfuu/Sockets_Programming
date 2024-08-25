e15477fdb81cc64d699c349a4d53d50dc354a299
Max Fu <mfu27@ucmerced.edu>
2024-08-24 18:21:20 -0700

    new file structure

---
+++ test/test_main.c
@@ -0,0 +1,13 @@
+// #include "test_utils.h" 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <sys/socket.h>
+// #include <arpa/inet.h> 
+
+// #define IP_SERVER "127.0.0.1" 
+// #define SERVER_PORT 8080 
+
+// int main() { 
+
+// }
--- test/test_utils.h
+++ test/test_utils.h
@@ -6,5 +6,4 @@ double measure_throughput(int sockfd, int data_size, int iterations);
 double measure_latency(int sockfd, int iterations); 
 double measure_error_rate(int sockfd, int data_size, int interations); 
 
-
 #endif 
